9503b07df2bbf22254b6dda577a36ea129a96419
Lunkubuntu <l.le.mi.lunk15@gmail.com>
2022-12-05 17:15:00 +0100

    Actividad 2.1 - Terminada

--- lucialm_login/login.sql
+++ lucialm_login/login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 04-12-2022 a las 23:53:00
+-- Tiempo de generación: 05-12-2022 a las 17:13:41
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 7.4.29
 
@@ -37,9 +37,10 @@ CREATE TABLE `imagen` (
 --
 
 INSERT INTO `imagen` (`nombre`, `nombre_usuario`) VALUES
-('sif.jpg', 'Könnar'),
-('Poster.png', 'Laiss'),
-('samsas.jpg', 'Ösdeln');
+('JADSLKDJSAKL.jpg', 'Frysha'),
+('robl.webp', 'Könnar'),
+('1635583371260.jpg', 'Laiss'),
+('a.jpg', 'Ösdeln');
 
 -- --------------------------------------------------------
 
@@ -61,10 +62,11 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`nombre`, `pass`, `savepath`, `sessionid`, `sessionname`) VALUES
 ('Ekaterini', '$2y$10$4RgONKsoO4QQSCkEcP13ee5bBVnWh3EWLlgJPqOEYQcTbp2Hqjuvq', '/opt/lampp/temp/', 'a2ced5c4a5359a8c81fc2adc1c84664c', 'Ekaterini'),
+('Frysha', '$2y$10$Rv3U1NNx8bUcbP/c4aeX7eWqITSl72j84nNShhPQTINmytli7PA96', '/opt/lampp/temp/', '5eae9df2cd039264d2a03605c2a89228', 'Frysha'),
 ('Iris', '$2y$10$4eq8KryY9x.QLP7KDAuDFOi0Od0CV/hZPdc9OC/sisghTmSdTE6Em', '/opt/lampp/temp/', '5764d683a4913c6397f65926f97add22', 'Iris'),
 ('Karrán', '$2y$10$mYjXYoIMBAgbgMjZ0DV//eoU4PiT6Jb7SVAlX3Ox6YLjIsPdFudcO', '/opt/lampp/temp/', '5da53fefd323227db6a14cc96fc0e54c', 'Karrán'),
 ('Könnar', '$2y$10$RBnnXnSXw8rw1ViBBssFbeTatYxCVHjJP4MqEAdf6gPqePgEoLUa.', '/opt/lampp/temp/', '7e2b612484c034240c47d62156119cf6', 'Könnar'),
-('Laiss', '$2y$10$t1yiZwjYoVtyq7cCVjzQM.8Q4XxTW8.P/e5RssjSfR.YbyD5rZpYG', '/opt/lampp/temp/', '3c1a2fee6960450811f24d6ad1ca9669', 'Laiss'),
+('Laiss', '$2y$10$WjWIQpDe5yTpuTAozqMrAOrnsNmxw7TwZA6aS1dqr/aWwutQTMUra', '/opt/lampp/temp/', '3c1a2fee6960450811f24d6ad1ca9669', 'Laiss'),
 ('Nosk', '$2y$10$ge15pWgweJ0HOkNnjd7oa.pThPbFDgaedHXCldUylIbtX6ObjbVtC', '/opt/lampp/temp/', '173fe10b7c5d1324c16211e598de0665', 'Notsk'),
 ('Ösdeln', '$2y$10$X0eu7pWc7aESqowWw.e2UO3RVlNm5dRlf83L6jCtUp6.bK9rxEVce', '/opt/lampp/temp/', '2b2076f18d39c84513fcf3d1c32b9086', 'Ösdeln'),
 ('Therthes', '$2y$10$VJUhGVskTgB.2blt6xdNIOyQPxBcGOMVBqr1e5aHUIV0AyNZNo57e', '/opt/lampp/temp/', 'ba542dd4de1068ecd7c1143f09f65910', 'PHPSESSID'),
